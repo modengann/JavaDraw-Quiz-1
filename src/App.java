23d7cd28c7aaed1411caaca388c03581cac51b94
modengann <99417989+modengann@users.noreply.github.com>
2022-10-12 09:58:22 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,35 @@
+/** 
+ *  Write code to:
+ *  1. Create a circle with a radius of 100. The circle can be 
+ *  located anywhere on the screen to start. 
+ *  
+ *  When the mouse is clicked: 
+ *  1. The color of the circle should change randomly.
+ *  2. The center of the circle should move to a location that 
+ *     is 1/2 of the x and y coordinates of the mouse's current location
+ * 
+ * For example:
+ * 
+ * If the mouse is clicked at (500, 200), the circle should change 
+ * its location so that its center is at (250, 100) because 500/2 = 250 and 
+ * 200/2 = 100. It should also change its color to a random color.
+ * 
+ * If the mouse is clicked at (88, 140) the circle should change its 
+ * location so that its center is at (44, 70) because 88/2 = 44 and 
+ * 140/2 = 70. Again the color should change randomly.
+ * 
+ * To earn an A+:
+ * 
+ * Complete all the above requirements and surprise me! Be creative in 
+ * showing me that you have a mastery of the JavaDraw concepts. This can 
+ * be anything that you'd like that you think is impressive!
+ */
+
+
+import javadraw.*;
+
+public class App extends Window{
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
